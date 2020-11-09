07fdd78b1d4400d0aa0a3d57ec5c6dbc4af2a81a
Shubh Bansal <73333148+sbwhjrCP@users.noreply.github.com>
2020-11-09 13:57:59 +0530

    Update Game.js

--- js/Game.js
+++ js/Game.js
@@ -1,104 +1,96 @@
 class Game {
-  constructor(){
-
-  }
-
-  getState(){
-    var gameStateRef  = database.ref('gameState');
-    gameStateRef.on("value",function(data){
-       gameState = data.val();
-    })
-
-  }
-
-  update(state){
-    database.ref('/').update({
-      gameState: state
+  getState(secret_word) {
+    var gameStateRef = db.ref(`users/${secret_word}/game_state/`);
+    gameStateRef.on("value", function (data) {
+      gameState = data.val();
     });
   }
 
-  async start(){
-    if(gameState === 0){
-      player = new Player();
-      var playerCountRef = await database.ref('playerCount').once("value");
-      if(playerCountRef.exists()){
+  update(state) {
+    db.ref(`users/${secret_word}/`).update({
+      game_state: state,
+    });
+  }
+
+  async start() {
+    // When user visit the page
+    if (gameState === null) {
+      welcome.display();
+    }
+
+    // //When user loged in successfully
+    if (gameState === 0) {
+      var playerCountRef = await db
+        .ref(`users/${secret_word}/player_count/`)
+        .once("value");
+
+      if (playerCountRef.exists()) {
         playerCount = playerCountRef.val();
         player.getCount();
       }
-      form = new Form()
-      form.display();
     }
-
-    car1 = createSprite(100,200);
-    car1.addImage("car1",car1_img);
-    car2 = createSprite(300,200);
-    car2.addImage("car2",car2_img);
-    car3 = createSprite(500,200);
-    car3.addImage("car3",car3_img);
-    car4 = createSprite(700,200);
-    car4.addImage("car4",car4_img);
-    cars = [car1, car2, car3, car4];
   }
 
-  play(){
-    form.hide();
-    
+  play() {
     Player.getPlayerInfo();
-    
-    if(allPlayers !== undefined){
-      background(rgb(198,135,103));
-      image(track, 0,-displayHeight*4,displayWidth, displayHeight*5);
-      
+    player.getCarsAtEnd();
+
+    if (allPlayers !== undefined) {
+      background("#464646");
+      image(track, 0, -height * 4, width, height * 5);
+
       //var display_position = 100;
-      
+
       //index of the array
       var index = 0;
 
       //x and y position of the cars
-      var x = 175 ;
+      var x = width / 2 - 680;
       var y;
 
-      for(var plr in allPlayers){
+      for (var plr in allPlayers) {
         //add 1 to the index for every loop
-        index = index + 1 ;
+        index = index + 1;
 
         //position the cars a little away from each other in x direction
-        x = x + 200;
+        x = x + 455;
         //use data form the database to display the cars in y direction
-        y = displayHeight - allPlayers[plr].distance;
-        cars[index-1].x = x;
-        cars[index-1].y = y;
+        y = height - allPlayers[plr].distance;
+        cars[index - 1].x = x;
+        cars[index - 1].y = y;
 
-        if (index === player.index){
-          text("Shubh",50,50);
+        if (index === player.index) {
           stroke(10);
           fill("red");
-          ellipse(x,y,60,60);
-
+          ellipse(x, y, 60, 60);
           cars[index - 1].shapeColor = "red";
-          camera.position.x = displayWidth/2;
-          camera.position.y = cars[index-1].y;
+          camera.position.x = width / 2;
+          camera.position.y = cars[index - 1].y;
         }
-       
-        //textSize(15);
-        //text(allPlayers[plr].name + ": " + allPlayers[plr].distance, 120,display_position)
       }
-
     }
 
-    if(keyIsDown(UP_ARROW) && player.index !== null){
-      player.distance +=10
+    if (keyIsDown(UP_ARROW) && player.index !== null) {
+      player.distance += 10;
       player.update();
     }
 
-    if(player.distance > 3860){
+    if (player.distance > height * 5 - 100) {
       gameState = 2;
+      player.rank += 1;
+      Player.updateCarsAtEnd(player.rank);
+      swal({
+        title: `Awesome!${"\n"}Rank${"\n"}${player.rank}`,
+        text: "You reached the finish line successfully",
+        imageUrl:
+          "https://raw.githubusercontent.com/vishalgaddam873/p5-multiplayer-car-race-game/master/assets/cup.png",
+        imageSize: "100x100",
+        confirmButtonText: "Ok",
+      });
     }
-   
+
     drawSprites();
   }
 
-  end(){
-    console.log("Game Ended");
-  }
+  end() {}
 }
