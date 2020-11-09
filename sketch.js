09bfe9a7f60d193c5d2df6c4c1a7fa75b4149d4a
Shubh Bansal <73333148+sbwhjrCP@users.noreply.github.com>
2020-11-09 13:53:06 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,63 +1,44 @@
-var canvas;
-var backgroundImage, track, car1_img, car2_img;
-var fireAuth, db;
-var game, welcome, teacher, student;
-var secret_word;
-var player, allPlayers;
-var gameState = null;
+var canvas, backgroundImage;
+
+var gameState = 0;
 var playerCount;
-function preload() {
-  backgroundImage = loadImage("./assets/bg.jpg");
-  track = loadImage("./assets/track.jpg");
-  car1_img = loadImage("./assets/car1.png");
-  car2_img = loadImage("./assets/car2.png");
+var allPlayers;
+var distance = 0;
+var database;
+
+var form, player, game;
+
+var cars, car1, car2, car3, car4;
+
+var track, car1_img, car2_img, car3_img, car4_img;
+
+function preload(){
+  track = loadImage("../assets/track.jpg");
+  car1_img = loadImage("../assets/car1.png");
+  car2_img = loadImage("../assets/car2.png");
+  car3_img = loadImage("../assets/car3.png");
+  car4_img = loadImage("../assets/car4.png");
+  ground = loadImage("../assets/ground.png");
 }
 
-function setup() {
-  canvas = createCanvas(displayWidth, displayHeight);
-  fireAuth = firebase.auth();
-  db = firebase.database();
+function setup(){
+  canvas = createCanvas(displayWidth - 20, displayHeight-30);
+  database = firebase.database();
   game = new Game();
-  welcome = new Welcome();
-  teacher = new Teacher();
-  student = new Student();
-  player = new Player();
-
-  car1 = createSprite(width / 2, 200);
-  car1.addImage("car1", car1_img);
-  car2 = createSprite(width - 300, 200);
-  car2.addImage("car2", car2_img);
-
-  cars = [car1, car2];
+  game.getState();
+  game.start();
 }
 
-function draw() {
-  background(backgroundImage);
-  if (gameState === null || gameState === 0) {
-    game.start();
-  }
-  if (playerCount === 2) {
+
+function draw(){
+  if(playerCount === 4){
     game.update(1);
   }
-
-  if (gameState === 1) {
+  if(gameState === 1){
     clear();
-    student.greeting.hide();
-    student.greeting2.hide();
-    student.playButton.hide();
-
-    teacher.greeting.hide();
-    teacher.greeting2.hide();
-    teacher.playButton.hide();
-    teacher.secretWord.hide();
-
     game.play();
   }
-  if (gameState === 2) {
+  if(gameState === 2){
     game.end();
   }
-}
-
-function windowResized() {
-  resizeCanvas(displayWidth, displayHeight);
 }
